3ea5e18bd3c7c1cb9049564e86098689fcfc14d2
Bhavishya_Jani <Bhavishyajani09@gmail.com>
2026-02-09 14:06:37 +0530

    Revert "backend dev15"
    This reverts commit 515a99fa06149bc0127b8677e441e44c9eb1cf31.

--- frontend/src/services/artistService.js
+++ frontend/src/services/artistService.js
@@ -1,9 +1,9 @@
-import { API_URL } from '../config';
+import { API_BASE_URL } from '../config';
 
 const artistService = {
     getArtists: async () => {
         try {
-            const response = await fetch(`${API_URL}/artists`);
+            const response = await fetch(`${API_BASE_URL}/artists`);
             const data = await response.json();
             return data;
         } catch (error) {
@@ -14,7 +14,7 @@ const artistService = {
 
     getArtistById: async (id) => {
         try {
-            const response = await fetch(`${API_URL}/artists/${id}`);
+            const response = await fetch(`${API_BASE_URL}/artists/${id}`);
             const data = await response.json();
             return data;
         } catch (error) {
